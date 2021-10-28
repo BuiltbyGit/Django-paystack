9054a9203afe561591bf8f6d0e7a8a9fb6845523
builtbygit <eorgestomper22@gmail.com>
2021-10-28 14:51:10 +0100

    Testing

--- hook/views.py
+++ hook/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django import template
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 import json
 
 # Create your views here.
@@ -11,6 +12,7 @@ import json
 
 # Create your views here.
 @csrf_exempt
+@require_POST
 def hook(request):
     # return HttpResponse('Transaction successful')
     if request.is_ajax():
