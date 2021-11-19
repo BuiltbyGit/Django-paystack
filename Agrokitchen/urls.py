98ad191b40ec0bd15adddb44d45bed26c9da9cc8
Georgie <30572471+BuiltbyGit@users.noreply.github.com>
2021-11-19 12:14:39 +0100

    Update urls.py

--- Agrokitchen/urls.py
+++ Agrokitchen/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('paymentprocess.urls')),x
+    path('', include('paymentprocess.urls')),
     path('hook', include('hook.urls'))
 ]
